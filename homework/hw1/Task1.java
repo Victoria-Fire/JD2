d58f56f82c859d66ae435a8a9bbdcf18995511e7
Victoria-Fire <vik.fire@yandex.ru>
2022-03-06 11:49:29 +0300

    Deleted "=)"

--- homework/hw1/Task1.java
+++ homework/hw1/Task1.java
@@ -2,6 +2,6 @@ package homework.hw1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello world =)");
+        System.out.println("Hello world");
     }
 }
